709022d023d18ee91ebbce53d16922f55874c0d2
Utkarsh Mishra <114844983+Utkarshmishra2k2@users.noreply.github.com>
2026-09-05 06:55:25 +0530

    Add files via upload

---
+++ Java-FullStack/JetleaseApplication.java
@@ -0,0 +1,11 @@
+package com.jetlease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetleaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JetleaseApplication.class, args);
+    }
+}
